41f9e3a533d1b5d95eb163273700ad3aaa4dc799
t.hayashi <t.hayashi0077+bitbucket@gmail.com>
2013-12-21 21:39:53 +0900

    Initial Commit

---
+++ iOS-Common/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Common
+//
+//  Created by 林 達也 on 2013/12/21.
+//  Copyright (c) 2013年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS-Common/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS-Common
+//
+//  Created by 林 達也 on 2013/12/21.
+//  Copyright (c) 2013年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
